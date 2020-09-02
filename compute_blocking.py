5ad4a111df5d6dbd4faa92f17060591b595709a6
unknown <hlc5v@virginia.edu>
2020-09-02 11:32:15 -0400

    Changed budget allocation.

--- compute_blocking.py
+++ compute_blocking.py
@@ -85,9 +85,18 @@ def main():
                         infected_2 = results['node_count'][2] + results['node_count'][3]
                         total_infected = sum(results['node_count'][i] for i in range(1, 4))
                         # Select nodes appropriately
-                        ratio = infected_1 / total_infected
-                        budget_1 = int(ratio * budget)
-                        budget_2 = budget - budget_1
+                        # Select nodes appropriately
+                        if infected_1 > infected_2:
+                            ratio_total = infected_1 / total_infected
+                            budget_1 = np.ceil(ratio_total * budget)
+                            budget_2 = budget - budget_1
+                        elif infected_1 < infected_2:
+                            ratio_total = infected_2 / total_infected
+                            budget_2 = np.ceil(ratio_total * budget)
+                            budget_1 = budget - budget_2
+                        else:
+                            budget_1 = budget // 2
+                            budget_2 = budget - budget_1
 
                         # Run through the CBH from DMKD for both contagions.
                         choices_1 = cbh.try_all_sets(node_infections_1, budget_1, model, seed_set, 1)
