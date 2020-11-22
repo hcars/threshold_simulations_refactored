8b16929384b5eb007e63906ad2222602a31853a6
hcars <hlc5v@virginia.edu>
2020-11-22 10:13:20 -0500

    Comparison drivers added.

--- julia_version/driver_jl_comp.sh
+++ julia_version/driver_jl_comp.sh
@@ -9,9 +9,9 @@ module load julia/1.5.0
 module load gurobi/9.0.1
 
 directory_structure="../complex_net_proposal/experiment_networks/"
-input_paths=("enron.giant.clean.uel" "astroph.edges" "wiki.edges" "fb-pages-politician.edges" "slashdot0811.edges")
+input_paths=("jazz.net.clean.uel")
 
-output_append="../complex_net_proposal/experiment_results/results_mcich_ilp.csv"
+output_append="../complex_net_proposal/experiment_results/results_comp.csv"
 
 random_seed=20591
 
@@ -19,14 +19,13 @@ repititions=100
 
 seed_method=$1
 
-blocking_method="MCICH_ILP"
 
 num_seeds=20
 for base in ${input_paths[@]};
 do
    full=$directory_structure
    full+=$base
-   julia main.jl $full $repititions $seed_method $num_seeds $random_seed $output_append $blocking_method
+   julia main.jl $full $repititions $seed_method $num_seeds $random_seed $output_append
 done
 
 sed -r -i  "s/.*experiment_networks\///g" $output_append
