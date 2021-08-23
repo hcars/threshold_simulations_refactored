28421f347788fbe96602f5af2d9a60e89bc2beb1
Henry Carscadden <hlc5v@virginia.edu>
2021-08-23 11:14:58 -0400

    dad

--- julia_version/DiffusionModel.jl
+++ julia_version/DiffusionModel.jl
@@ -136,22 +136,24 @@ function iterate!(model::MultiDiffusionModel)::Tuple
 			if (u_state == 1)
 				interaction_term_2_1 = model.ξ_i[2]
 			end
-            println(interaction_term_1_2)
 
-            transition_1 = (cnt_infected_1 >= (get(
+            thres_1 = get(
                 model.thresholdStates,
                 u,
                 model.θ_i[1],
-            ) - interaction_term_1_2 )) &&
+            ) - interaction_term_1_2 )
+            transition_1 = (cnt_infected_1 >= thres_1) &&
                            ((get(model.blockedDict, u, 0) != 1) &&
                             (get(model.blockedDict, u, 0) != 3))
-            transition_2 = (cnt_infected_2 >= (get(
+            thres_2 = get(
                 model.thresholdStates,
                 u,
                 model.θ_i[2],
-            ) - interaction_term_2_1)) &&
+            ) - interaction_term_2_1
+            transition_2 = (cnt_infected_2 >= thres_2) &&
                            ((get(model.blockedDict, u, 0) != 2) &&
                             (get(model.blockedDict, u, 0) != 3))
+            println(thres_2A)
             old_state = u_state
             if ((transition_1 == true) && (old_state != 1))
                 get!(updated_1, u, cnt_infected_1)
